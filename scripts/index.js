a74f70db2326ffa2393e7718a1e7e02063865599
Alexader Dolgov <aleksandrdolgov188@gmail.com>
2025-02-19 13:17:39 +0300

    Проба пера

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,68 @@
 // @todo: Темплейт карточки
 
+    function cardDelet (deletButton) {
+        let delet = deletButton.target.closest('.card');
+        delet.remove(delet);
+    }
+
+   
+
+   
+
+    
+    function createСard (initialCards,cardDelet) {
+        
+        let cardTemplate = document.querySelector('#card-template').content;
+        let userElement = cardTemplate.querySelector('.card').cloneNode(true);
+        
+
+        userElement.querySelector('.card__image').src = initialCards.link;
+        userElement.querySelector('.card__title').textContent = initialCards.name;
+
+        cardItem.append(userElement);
+
+
+        let deletButton = userElement.querySelector('.card__delete-button');
+        deletButton.addEventListener('click', cardDelet);
+        
+    }
+    
+    let cardItem = document.querySelector('.places__list');
+    
+    initialCards.forEach(function(element) {
+        createСard(element,cardDelet)
+    });
+        
+    
+
+    
+   
+
+    
+
+
+    // for (let i = 0; i < initialCards.length; i++) {
+
+    //     let cardTemplate = document.querySelector('#card-template').content;
+    //     let userElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    //     userElement.querySelector('.card__image').src = initialCards[i].link;
+    //     userElement.querySelector('.card__title').textContent = initialCards[i].name;
+
+    //     cardItem.append(userElement);
+        
+    // }
+
+    
+
+    
+   
+
+
+
+
+
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
