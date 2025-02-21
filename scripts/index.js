5551bec7aacab0ebdba875beb43f2b9f24ceb12d
Alexader Dolgov <aleksandrdolgov188@gmail.com>
2025-02-21 13:14:31 +0300

    Попытка номер 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 // @todo: Темплейт карточки
 
 function deleteCard(deleteButton) {
-  let closestCard = deleteButton.target.closest(".card");
+  const closestCard = deleteButton.target.closest(".card");
   closestCard.remove();
 }
 
-let cardsContainer = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 
 function createСard(cardData, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
-  let cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-
-  cardElement.querySelector(".card__image").src = cardData.link;
-  cardElement.querySelector(".card__image").alt = cardData.name;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   cardElement.querySelector(".card__title").textContent = cardData.name;
 
-  let deleteButton = cardElement.querySelector(".card__delete-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", deleteCard);
 
   return cardElement;
