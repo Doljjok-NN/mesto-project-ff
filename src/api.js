b646bf8131c23ae1f51b327a6d564386915fc3b7
Alexader Dolgov <aleksandrdolgov188@gmail.com>
2025-04-02 14:41:29 +0300

    Save

---
+++ src/api.js
@@ -0,0 +1,50 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/wff-cohort-35",
+  headers: {
+    authorization: "7bda876f-318f-4941-a0cf-9029fab62bbc",
+    "Content-Type": "application/json",
+  },
+};
+
+// GET - запрос данных
+// POST - отправка данных
+// PUT - заменна данных на сервере
+// PATCH - частичная замена
+// DELETE - удаление данных
+// HEAD - запросы только заголовков
+
+// запрос сервера
+function getResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`ошибка ${res.status}`);
+};
+
+// запрос профиля
+export function getProfil() {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "GET",
+    headers: config.headers,
+  }).then(getResponse);
+}
+
+//Редактирование профиля
+export function getProfileEdit(name, about) {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify({
+      name: name,
+      about: about,
+    }),
+  }).then(getResponse);
+}
+
+// запрос массива карточек
+export function getCard () {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "GET",
+    headers: config.headers,
+  }).then(getResponse);
+};
--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import "./index.css";
 import { initialCards } from "./cards.js";
 import { createСard, deleteCard, likeCard } from "./card.js";
 import { closeModal, openModal } from "./modal.js";
+import { enableValidation } from "./validate.js";
+import { getProfil, getProfileEdit, getCard } from "./api.js";
+import { data } from "autoprefixer";
 
 // ------------------СОЗДАНИЕ КАРТОЧКИ----------------------------------------------
 const cardsContainer = document.querySelector(".places__list");
 
-initialCards.forEach(function (cardData) {
-  const newCard = createСard(cardData, deleteCard, likeCard, openImageModal);
-
-  cardsContainer.append(newCard);
-});
+getCard()
+  .then((cardArray) => {
+    cardArray.reverse();
+    cardArray.forEach((card) => {
+      const newCard = createСard(card, deleteCard, likeCard, openImageModal);
+      cardsContainer.append(newCard);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //------------------------- ОТКРЫТИЕ И ЗАКРЫТИЕ ПОПАПОВ НА КРЕСТИК---------------------
 const popup = document.querySelectorAll(".popup");
@@ -36,25 +45,31 @@ imgModalClose.addEventListener("click", () => closeModal(popupImageModalka));
 // ---------------------------------ФОРМА-----------------------------------------------------
 const formEditProfile = document.querySelector("#profile_form");
 const formNameInput = formEditProfile.querySelector(".popup__input_type_name");
-const formWorkInput = formEditProfile.querySelector(
-  ".popup__input_type_description"
-);
+const formWorkInput = formEditProfile.querySelector(".popup__input_type_description");
+const inputName = document.querySelector(".profile__title");
+const inputWork = document.querySelector(".profile__description");
 
-function submitEditProfileForm(evt) {
+function profileSubmit(evt) {
   evt.preventDefault();
-
-  const valueName = formNameInput.value;
-  const valueWork = formWorkInput.value;
-
-  const inputName = document.querySelector(".profile__title");
-  const inputWork = document.querySelector(".profile__description");
-
-  inputName.textContent = valueName;
-  inputWork.textContent = valueWork;
-
-  closeModal(popupTypeEdit);
+  getProfileEdit(formNameInput.value, formWorkInput.value)
+    .then((data) => {
+      inputName.textContent = data.name;
+      inputWork.textContent = data.about;
+      closeModal(popupTypeEdit);
+    })
+    .catch((err) => {
+      console.log(`Ошибка ${err}`);
+    });
 }
-formEditProfile.addEventListener("submit", submitEditProfileForm);
+
+formEditProfile.addEventListener("submit", profileSubmit);
+
+// -----------------ПОЛУЧЕНИЕ ДАННЫХ ФОРМЫ С СЕРВЕРА---------------------
+function init(users) {
+  inputName.textContent = users.name;
+  inputWork.textContent = users.about;
+}
+getProfil().then(init);
 
 //-------------------------ДОБАВЛЕНИЕ КАРТОЧКИ-----------------------------------------------
 const formNewCard = document.getElementById("form_card");
@@ -87,3 +102,7 @@ function openImageModal(styleImag, textImg) {
   imageModalText.textContent = textImg;
   openModal(popupImageModalka);
 }
+
+// ----------------------------ВАЛИДАЦИЯ----------------------------------------
+enableValidation();
+
